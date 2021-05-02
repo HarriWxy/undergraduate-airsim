cd7b65afb6febc0e0185c10b0cf042de6ef5d68a
WangHyscuaa <316361680@qq.com>
2021-05-02 09:33:26 +0800

    0502

--- flappybird/deep_q_network.py
+++ flappybird/deep_q_network.py
@@ -5,7 +5,7 @@ from __future__ import print_function
 import tensorflow as tf
 
 from tensorflow.keras import Model
-from tensorflow.keras.layers import Conv2D, Activation, MaxPool2D, Flatten, Dense, Dropout
+from tensorflow.keras.layers import Conv2D, Activation, MaxPool2D, Flatten, Dense, Dropout, LSTM
 
 import cv2
 import sys
@@ -24,10 +24,10 @@ GAME = 'FlappyBird' # 游戏名称
 ACTIONS = 2 # 2个动作数量
 ACTIONS_NAME=['不动','起飞']  #动作名
 GAMMA = 0.99 # 未来奖励的衰减
-OBSERVE = 1000. # 训练前观察积累的轮数
+OBSERVE = 1000 # 训练前观察积累的轮数
 EPSILON = 1
-REPLAY_MEMORY = 3600 # 观测存储器D的容量
-BATCH = 36 # 训练batch大小
+REPLAY_MEMORY = OBSERVE # 观测存储器D的容量
+BATCH = 4 # 训练batch大小
 TIMES = 500000
 
 class MyNet(Model):
@@ -36,7 +36,6 @@ class MyNet(Model):
         self.c1_1 = Conv2D(filters=32, kernel_size=(8, 8),strides=4, padding='same',
                            kernel_initializer=tf.keras.initializers.TruncatedNormal(mean=0.0, stddev=0.01, seed=None),
                            bias_initializer = tf.keras.initializers.Constant(value=0.01))  # 卷积层
-        #self.b1 = BatchNormalization()  # BN层
         self.a1_1 = Activation('relu')  # 激活层
         self.p1 = MaxPool2D(pool_size=(2, 2), strides=2, padding='same')  # 池化层
         self.d1 = Dropout(0.2)  # dropout层
@@ -44,19 +43,18 @@ class MyNet(Model):
         self.c2_1 = Conv2D(filters=64, kernel_size=(4, 4),strides=2, padding='same',
                            kernel_initializer=tf.keras.initializers.TruncatedNormal(mean=0.0, stddev=0.01, seed=None),
                            bias_initializer = tf.keras.initializers.Constant(value=0.01))  # 卷积层
-        #self.b1 = BatchNormalization()  # BN层
         self.a2_1 = Activation('relu')  # 激活层
         self.c2_2 = Conv2D(filters=64, kernel_size=(3, 3),strides=1, padding='same',
                            kernel_initializer=tf.keras.initializers.TruncatedNormal(mean=0.0, stddev=0.01, seed=None),
                            bias_initializer = tf.keras.initializers.Constant(value=0.01))  # 卷积层
-        #self.b1 = BatchNormalization()  # BN层
         self.a2_2 = Activation('relu')  # 激活层
         self.p2 = MaxPool2D(pool_size=(2, 2), strides=2, padding='same')  # 池化层
 
         self.flatten = Flatten()
-        self.f1 = Dense(512, activation='relu',
+        self.f1 = Dense(256, activation='relu',
                            kernel_initializer=tf.keras.initializers.TruncatedNormal(mean=0.0, stddev=0.01, seed=None),
                            bias_initializer = tf.keras.initializers.Constant(value=0.01))
+        self.l1 = LSTM(256)
         self.f2 = Dense(ACTIONS, activation=None,
                            kernel_initializer=tf.keras.initializers.TruncatedNormal(mean=0.0, stddev=0.01, seed=None),
                            bias_initializer = tf.keras.initializers.Constant(value=0.01))
@@ -76,8 +74,11 @@ class MyNet(Model):
 
         x = self.flatten(x)
         x = self.f1(x)
+        x = tf.expand_dims(x,axis=0)
+        x = self.l1(x)
         y = self.f2(x)
         return y
+
 
 def trainNetwork(istrain):
 #============================ 模型创建与加载 ===========================================
@@ -118,15 +119,16 @@ def trainNetwork(istrain):
     #初始化状态并且预处理图片，把连续的四帧图像作为一个输入（State）
     do_nothing = np.zeros(ACTIONS)
     do_nothing[0] = 1
-    s_t, r_0, terminal, _ = game_state.frame_step(do_nothing)
-    #print(s_t.shape)
+    x_t, r_0, terminal, _ = game_state.frame_step(do_nothing)
+    s_t = np.stack((x_t,x_t,x_t,x_t,x_t),axis=0)
+
 
     # 开始训练
     while t < TIMES+1:
         # 根据输入的s_t,选择一个动作a_t
         epsilon = EPSILON - (EPSILON-0.1)*t/TIMES # 网络的过早介入会导致
         # 学习率
-        learning_r=0.03-(0.03-0.00025)*t/TIMES
+        learning_r=0.02-(0.02-0.00025)*t/TIMES
         optimizer=tf.keras.optimizers.RMSprop(learning_r,0.99,0.0,1e-7)
         
         a_t_to_game = np.zeros([ACTIONS])
@@ -139,14 +141,16 @@ def trainNetwork(istrain):
             a_t_to_game[action_index] = 1
         else:
             print("-----------net choice----------------")
-            readout_t = net1(tf.expand_dims(tf.constant(s_t, dtype=tf.float32), 0))
+            readout_t = net1(tf.constant(s_t, dtype=tf.float32))
             action_index = np.argmax(readout_t)
             print("-----------index----------------")
             print(action_index)
             a_t_to_game[action_index] = 1
 
         #执行这个动作并观察下一个状态以及reward
-        s_t1, r_t, terminal, score = game_state.frame_step(a_t_to_game)
+        x_t, r_t, terminal, score = game_state.frame_step(a_t_to_game)
+        x_t = x_t[np.newaxis,:]
+        s_t = np.concatenate((s_t[1:],x_t))
         print("============== score ====================")
         print(score)
         Score+=score
@@ -170,20 +174,18 @@ def trainNetwork(istrain):
 
         # a_t = action_index
 
-        s_t = tf.convert_to_tensor(s_t, dtype=tf.float32)
-        action_index = tf.constant(action_index, dtype=tf.int32)
+        x_t = tf.convert_to_tensor(x_t, dtype=tf.float32)
+        # action_index = tf.constant(action_index, dtype=tf.int32)
         r_t = tf.constant(r_t, dtype=tf.float32)
-        s_t1 = tf.constant(s_t1, dtype=tf.float32)
         terminal = tf.constant(terminal, dtype=tf.float32)
 
         # 将观测值存入之前定义的观测存储器D中
-        D.append((s_t, action_index, r_t, s_t1, terminal))
+        D.append((x_t, action_index, r_t, terminal))
         #如果D满了就替换最早的观测
         if len(D) > REPLAY_MEMORY:
             D.popleft()
 
         # 更新状态，不断迭代
-        s_t = s_t1
         t += 1
 
 #============================ 训练网络 ===========================================
@@ -192,45 +194,27 @@ def trainNetwork(istrain):
         if (t > OBSERVE) and istrain:
             # 随机抽取minibatch个数据训练
             print("==================start train====================")
-            
-            minibatch = random.sample(D, BATCH)
-
-            # 获得batch中的每一个变量
-            b_s = []
-            b_a = []
-            b_r = []
-            b_s_ = []
-            b_done = []
-            for d in minibatch:
-                b_s.append(d[0])
-                b_a.append(d[1])
-                b_r.append(d[2])
-                b_s_.append(d[3])
-                b_done.append(d[4])
-
-            b_s = tf.stack(b_s, axis=0)
-            b_a = tf.expand_dims(b_a, axis=1) # 将一维的动作索引扩展成二维，每一个是独立的
-            b_a = tf.stack(b_a, axis=0)
-            b_r = tf.stack(b_r, axis=0)
-            b_s_ = tf.stack(b_s_, axis=0)
-            b_done = tf.stack(b_done, axis=0)
-
-    
-
-            # 训练
             with tf.GradientTape() as tape:
-                q_output = net1(b_s)
-                index = tf.expand_dims(tf.range(0, BATCH), 1) # index的二维向量
-                # DDQN
-                q_next = netstar(b_s_) # 每一行出一个最大值
-                
-                index_b_a = tf.concat((index, b_a), axis=1) # [batch_index,action_index]
-                q = tf.gather_nd(q_output, index_b_a) # 当前状态下选择这个动作得到的Q值
-                q_next = tf.gather_nd(q_next,index_b_a)
+                loss=0
+                s_t = tf.constant(s_t, dtype=tf.float32)
+                q =  tf.reduce_max(net1(s_t[:4]),axis=1)
+                q_next = netstar(s_t[1:5])[0][action_index] # 每一行出一个最大值
+                q_truth = r_t + GAMMA * q_next* (tf.ones(1) - terminal)
+                loss += tf.losses.MSE(q_truth, q)
 
-                q_truth = b_r + GAMMA * q_next* (tf.ones(BATCH) - b_done)
-                
-                loss = tf.losses.MSE(q_truth, q)
+                # minibatch
+                for i in random.sample(range(REPLAY_MEMORY - 6), BATCH-1): 
+                    b_s = tf.concat((D[i][0],D[i+1][0],D[i+2][0],D[i+3][0],D[i+4][0]),axis=0)
+                    b_a = D[i+3][1]
+                    b_r = D[i+3][2]
+                    b_done = D[i+3][3]
+                    q = tf.reduce_max(net1(b_s[:4]),axis=1)
+                    q_next = netstar(b_s[1:5])[0][b_a] # 每一行出一个最大值
+                    q_truth = b_r + GAMMA * q_next* (tf.ones(1) - b_done)
+                    loss += tf.losses.MSE(q_truth, q)
+
+                # 训练
+                # loss = loss/BATCH
                 Loss += loss
                 print("loss = %f" % loss)
                 gradients = tape.gradient(loss, net1.trainable_variables)
